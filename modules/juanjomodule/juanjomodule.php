36233e14b92bf18e4b4d4eb4d01309f151b35317
Juan José <diazsantanajuanjose@gmail.com>
2022-12-17 23:50:14 +0000

    git ignore

--- modules/juanjomodule/juanjomodule.php
+++ modules/juanjomodule/juanjomodule.php
@@ -28,7 +28,7 @@ if (!defined('_PS_VERSION_')) {
     exit;
 }
 
-class Juanjomodule extends Module
+class JuanjoModule extends Module
 {
     protected $config_form = false;
 
@@ -61,14 +61,12 @@ class Juanjomodule extends Module
      */
     public function install()
     {
-        Configuration::updateValue('JUANJOMODULE_LIVE_MODE', false);
+        //Configuration::updateValue('JUANJOMODULE_LIVE_MODE', false);
 
-        include(dirname(__FILE__).'/sql/install.php');
+        //include(dirname(__FILE__).'/sql/install.php');
 
         return parent::install() &&
-            $this->registerHook('header') &&
-            $this->registerHook('displayBackOfficeHeader') &&
-            $this->registerHook('displayAdminOrderContentOrder');
+            $this->registerHook('actionGetAdminOrderButtons');
     }
 
     public function uninstall()
@@ -83,101 +81,101 @@ class Juanjomodule extends Module
     /**
      * Load the configuration form
      */
-    public function getContent()
-    {
-        /**
-         * If values have been submitted in the form, process.
-         */
-        if (((bool)Tools::isSubmit('submitJuanjomoduleModule')) == true) {
-            $this->postProcess();
-        }
-
-        $this->context->smarty->assign('module_dir', $this->_path);
-
-        $output = $this->context->smarty->fetch($this->local_path.'views/templates/admin/configure.tpl');
-
-        return $output.$this->renderForm();
-    }
+//    public function getContent()
+//    {
+//        /**
+//         * If values have been submitted in the form, process.
+//         */
+//        if (((bool)Tools::isSubmit('submitJuanjomoduleModule')) == true) {
+//            $this->postProcess();
+//        }
+//
+//        $this->context->smarty->assign('module_dir', $this->_path);
+//
+//        $output = $this->context->smarty->fetch($this->local_path.'views/templates/admin/configure.tpl');
+//
+//        return $output.$this->renderForm();
+//    }
 
     /**
      * Create the form that will be displayed in the configuration of your module.
      */
-    protected function renderForm()
-    {
-        $helper = new HelperForm();
-
-        $helper->show_toolbar = false;
-        $helper->table = $this->table;
-        $helper->module = $this;
-        $helper->default_form_language = $this->context->language->id;
-        $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG', 0);
-
-        $helper->identifier = $this->identifier;
-        $helper->submit_action = 'submitJuanjomoduleModule';
-        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false)
-            .'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
-        $helper->token = Tools::getAdminTokenLite('AdminModules');
-
-        $helper->tpl_vars = array(
-            'fields_value' => $this->getConfigFormValues(), /* Add values for your inputs */
-            'languages' => $this->context->controller->getLanguages(),
-            'id_language' => $this->context->language->id,
-        );
-
-        return $helper->generateForm(array($this->getConfigForm()));
-    }
+//    protected function renderForm()
+//    {
+//        $helper = new HelperForm();
+//
+//        $helper->show_toolbar = false;
+//        $helper->table = $this->table;
+//        $helper->module = $this;
+//        $helper->default_form_language = $this->context->language->id;
+//        $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG', 0);
+//
+//        $helper->identifier = $this->identifier;
+//        $helper->submit_action = 'submitJuanjomoduleModule';
+//        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false)
+//            .'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
+//        $helper->token = Tools::getAdminTokenLite('AdminModules');
+//
+//        $helper->tpl_vars = array(
+//            'fields_value' => $this->getConfigFormValues(), /* Add values for your inputs */
+//            'languages' => $this->context->controller->getLanguages(),
+//            'id_language' => $this->context->language->id,
+//        );
+//
+//        return $helper->generateForm(array($this->getConfigForm()));
+//    }
 
     /**
      * Create the structure of your form.
      */
-    protected function getConfigForm()
-    {
-        return array(
-            'form' => array(
-                'legend' => array(
-                'title' => $this->l('Settings'),
-                'icon' => 'icon-cogs',
-                ),
-                'input' => array(
-                    array(
-                        'type' => 'switch',
-                        'label' => $this->l('Live mode'),
-                        'name' => 'JUANJOMODULE_LIVE_MODE',
-                        'is_bool' => true,
-                        'desc' => $this->l('Use this module in live mode'),
-                        'values' => array(
-                            array(
-                                'id' => 'active_on',
-                                'value' => true,
-                                'label' => $this->l('Enabled')
-                            ),
-                            array(
-                                'id' => 'active_off',
-                                'value' => false,
-                                'label' => $this->l('Disabled')
-                            )
-                        ),
-                    ),
-                    array(
-                        'col' => 3,
-                        'type' => 'text',
-                        'prefix' => '<i class="icon icon-envelope"></i>',
-                        'desc' => $this->l('Enter a valid email address'),
-                        'name' => 'JUANJOMODULE_ACCOUNT_EMAIL',
-                        'label' => $this->l('Email'),
-                    ),
-                    array(
-                        'type' => 'password',
-                        'name' => 'JUANJOMODULE_ACCOUNT_PASSWORD',
-                        'label' => $this->l('Password'),
-                    ),
-                ),
-                'submit' => array(
-                    'title' => $this->l('Save'),
-                ),
-            ),
-        );
-    }
+//    protected function getConfigForm()
+//    {
+//        return array(
+//            'form' => array(
+//                'legend' => array(
+//                'title' => $this->l('Settings'),
+//                'icon' => 'icon-cogs',
+//                ),
+//                'input' => array(
+//                    array(
+//                        'type' => 'switch',
+//                        'label' => $this->l('Live mode'),
+//                        'name' => 'JUANJOMODULE_LIVE_MODE',
+//                        'is_bool' => true,
+//                        'desc' => $this->l('Use this module in live mode'),
+//                        'values' => array(
+//                            array(
+//                                'id' => 'active_on',
+//                                'value' => true,
+//                                'label' => $this->l('Enabled')
+//                            ),
+//                            array(
+//                                'id' => 'active_off',
+//                                'value' => false,
+//                                'label' => $this->l('Disabled')
+//                            )
+//                        ),
+//                    ),
+//                    array(
+//                        'col' => 3,
+//                        'type' => 'text',
+//                        'prefix' => '<i class="icon icon-envelope"></i>',
+//                        'desc' => $this->l('Enter a valid email address'),
+//                        'name' => 'JUANJOMODULE_ACCOUNT_EMAIL',
+//                        'label' => $this->l('Email'),
+//                    ),
+//                    array(
+//                        'type' => 'password',
+//                        'name' => 'JUANJOMODULE_ACCOUNT_PASSWORD',
+//                        'label' => $this->l('Password'),
+//                    ),
+//                ),
+//                'submit' => array(
+//                    'title' => $this->l('Save'),
+//                ),
+//            ),
+//        );
+//    }
 
     /**
      * Set values for the inputs.
@@ -194,37 +192,37 @@ class Juanjomodule extends Module
     /**
      * Save form data.
      */
-    protected function postProcess()
-    {
-        $form_values = $this->getConfigFormValues();
-
-        foreach (array_keys($form_values) as $key) {
-            Configuration::updateValue($key, Tools::getValue($key));
-        }
-    }
+//    protected function postProcess()
+//    {
+//        $form_values = $this->getConfigFormValues();
+//
+//        foreach (array_keys($form_values) as $key) {
+//            Configuration::updateValue($key, Tools::getValue($key));
+//        }
+//    }
 
     /**
     * Add the CSS & JavaScript files you want to be loaded in the BO.
     */
-    public function hookDisplayBackOfficeHeader()
-    {
-        if (Tools::getValue('configure') == $this->name) {
-            $this->context->controller->addJS($this->_path.'views/js/back.js');
-            $this->context->controller->addCSS($this->_path.'views/css/back.css');
-        }
-    }
+//    public function hookDisplayBackOfficeHeader()
+//    {
+//        if (Tools::getValue('configure') == $this->name) {
+//            $this->context->controller->addJS($this->_path.'views/js/back.js');
+//            $this->context->controller->addCSS($this->_path.'views/css/back.css');
+//        }
+//    }
 
     /**
      * Add the CSS & JavaScript files you want to be added on the FO.
      */
-    public function hookHeader()
-    {
-        $this->context->controller->addJS($this->_path.'/views/js/front.js');
-        $this->context->controller->addCSS($this->_path.'/views/css/front.css');
-    }
+//    public function hookHeader()
+//    {
+//        $this->context->controller->addJS($this->_path.'/views/js/front.js');
+//        $this->context->controller->addCSS($this->_path.'/views/css/front.css');
+//    }
 
-    public function hookDisplayAdminOrderContentOrder()
+    public function hookActionGetAdminOrderButtons()
     {
-        /* Place your code here. */
+        echo 'Hello World';
     }
 }
